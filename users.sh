45871512e922d9194edcf4783135b6cffad41d69
David Liptai <dliptai@swin.edu.au>
2022-11-29 11:13:16 +1100

    make sure new users are in 'users' group

--- users.sh
+++ users.sh
@@ -4,9 +4,10 @@
 # remember to tell students to change password immediately
 
 for user in $@; do
-  pswd="$(pwgen -cn 10 1)"
+  pswd="$(pwgen -cn 12 1)"
   adduser --quiet --disabled-password --gecos "" $user
   if [ "$?" -eq "0" ]; then
+    usermod $user -a -G users
     echo "${user}:${pswd}" | chpasswd
     echo $user $pswd
   fi
